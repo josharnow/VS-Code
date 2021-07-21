cf1c4d6df1b2c41843c61dd3a9173bea3a1dcc24
Joshua D. Arnow <1476967+josharnow@users.noreply.github.com>
2021-07-20 23:25:04 -0400

    Commit

---
+++ Coding Dojo/JavaScript/Arnow_Joshua_JS_Tinkering.js
@@ -0,0 +1,10 @@
+var childHeight = 60;
+function displayIfChildIsAbleToRideTheRollerCoaster() {
+    if (childHeight > 52) {
+        console.log("Get on that ride, kiddo!");
+    } else {
+        console.log("Sorry kiddo. Maybe next year.");
+    }
+displayIfChildIsAbleToRideTheRollerCoaster();
+}
+
