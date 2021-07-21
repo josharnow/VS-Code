b4c4cd13eb3799d9a4d78f3bdf7c36c75d66af7e
Joshua D. Arnow <1476967+josharnow@users.noreply.github.com>
2021-07-20 23:25:56 -0400

    Commit

--- Coding Dojo/Algorithms/w1d2.js
+++ Coding Dojo/Algorithms/w1d2.js
@@ -1,8 +1,32 @@
 // predict the output 1
-var a = 25;
-a = a - 13;
-console.log(a/2);
+// var a = 25;
+// a = a - 13;
+// console.log(a/2);
 
-a = "hello";
-console.log(a + "hello");
-// 6 hello hello 
+// a = "hello";
+// console.log(a + "hello");
+// // 6 hello hello
+
+// for(var i=0; i<10; i++) {
+//     console.log(i);
+//     i = i + 3;
+// }
+
+// console.log("outside of the loop" + i);
+
+function getTotal(arrayOfNumbers) {
+
+    var sum = arrayOfNumbers[0];
+
+    for(var i=0; i<arrayOfNumbers.length; i++) {
+        sum += arrayOfNumbers[i]
+        console.log("the current sum is: " + sum);
+    }
+    console.log("the total is: " + sum);
+
+}
+
+getTotal([1,3,5]);
+// sum = 1 --> 2 --> 5 --> 10
+// i = 0 --> 1 --> 2 --> 3
+// arrayOfNumbers.length = 3
---
+++ Coding Dojo/Web Fundamentals/JavaScript/Arnow_Joshua_JS_Tinkering.js
@@ -0,0 +1,9 @@
+var childHeight = 60;
+function displayIfChildIsAbleToRideTheRollerCoaster() {
+    if (childHeight > 52) {
+        console.log("Get on that ride, kiddo!");
+    } else {
+        console.log("Sorry kiddo. Maybe next year.");
+    }
+displayIfChildIsAbleToRideTheRollerCoaster();
+}
