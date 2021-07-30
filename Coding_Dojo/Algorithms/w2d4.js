a53c96d10ba84dfb4e8a1627231feaa7a947f624
Joshua D. Arnow <1476967+josharnow@users.noreply.github.com>
2021-07-30 00:37:34 -0400

    "Liking Likes" Part II

---
+++ Coding_Dojo/Algorithms/w2d4.js
@@ -0,0 +1,51 @@
+// var arr = [
+//     [2,7,1],
+//     [9,0,-13],
+//     [7,7,2],
+// ]
+
+// console.log(arr[1][2]);
+
+// function isPresent(arr2d, value) {
+//     // loop through the arrays in arr2d, arr2d.length
+//     for (let i = 0; i < arr2d.length; ++i) {
+//         for (let j = 0; j < arr2d[i].length; ++j) {
+//             if (arr2d[i][j] === value) {
+//                 return true;
+//             }
+            
+//         }
+        
+//     }
+
+//     return false;
+        // loop through the actual values of the current array
+        // check if number at current index in current array is equal to value
+            // if it is, return true
+    // else, return false
+// }
+
+// console.log(isPresent(arr,0));
+
+function flatten(arr2d) {
+    var flat = [];    
+}
+
+var result = flatten([[2, 5, 8], [3, 6, 1], [5, 7, 7]]);
+
+for (let k = 0; k < flatten.length; k++) {
+    if (flatten[k] === ) {
+        flat.push();
+    }
+    else{
+        return flat;
+    }
+    
+}
+
+// function flatten(arr2d) {
+//     var flat = [];
+//     // 
+//     for
+    
+// }
---
+++ Coding_Dojo/Web_Fundamentals/Learn-Platform-Assignments/JavaScript/Liking-Likes/Part-II/script.js
@@ -0,0 +1,20 @@
+var likeCount1 = document.querySelector("#likeCount1");
+
+function likeUp1() {
+    console.log(likeCount1);
+    likeCount1.innerText++;
+}
+
+var likeCount2 = document.querySelector("#likeCount2");
+
+function likeUp2() {
+    console.log(likeCount2);
+    likeCount2.innerText++;
+}
+
+var likeCount3 = document.querySelector("#likeCount3");
+
+function likeUp3() {
+    console.log(likeCount3);
+    likeCount3.innerText++;
+}
