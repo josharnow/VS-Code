55eb1013e8f26707bcd54610169f0eeea87ecd10
Joshua D. Arnow <1476967+josharnow@users.noreply.github.com>
2021-10-06 17:17:37 -0400

    "Daikichi Path Variables"

--- Coding_Dojo/Java/daikichiroutes/src/main/java/com/codingdojo/daikichiroutes/controllers/DaikichiController.java
+++ Coding_Dojo/Java/daikichiroutes/src/main/java/com/codingdojo/daikichiroutes/controllers/DaikichiController.java
@@ -1,4 +1,5 @@
 package com.codingdojo.daikichiroutes.controllers;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,14 +8,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class DaikichiController {
     @RequestMapping("")
     public String welcome() {
-            return "Welcome!";
+    	return "Welcome!";
     }
     @RequestMapping("/today")
     public String today() {
-            return "Today you will find luck in all your endeavors!";
+    	return "Today you will find luck in all your endeavors!";
     }
     @RequestMapping("/tomorrow")
     public String tomorrow() {
-            return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+    	return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
     }
+    @RequestMapping("/travel/{destination}")
+    public String travel(@PathVariable("destination") String location) {
+    	return "Congratulations! You will soon travel to " + location + "!";
+    }
+    @RequestMapping("/lotto/{num}")
+    public String lotto(@PathVariable("num") int num) {
+    	if (num % 2 == 0) {    		
+    		return "You will take a grand journey in the near future, but be weary of tempting offers.";
+    	} else {
+    		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+    	}
+    }
+    
 }
